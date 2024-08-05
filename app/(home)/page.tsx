1a3de3caede63e225656f814c7b5167415539d4d
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-08-05 06:45:29 -0700

    DATA FETCHING

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
 
-export default function Page() {
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return fetch(API_URL).then((response) => response.json());
+}
+
+export default async function Page() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>Hello</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,28 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log(props);
-  return <h1>Movie</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
